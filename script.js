78bd185ba39948c57b3aa39b50b65636a47327b2
Anugrah Jihad <153603783+AnugrahJihad@users.noreply.github.com>
2024-12-04 21:25:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Atribut container1 dan container2
+const bgColor = document.querySelectorAll("#container1, #container2")
+for (let i = 0; i < bgColor.length; i++) {
+  bgColor[i].style.backgroundColor = "#00FFFF";
+}
+
+
+// Atribut container3
+const lisCont3 = document.getElementsByTagName("li");
+for (let i = 0; i < lisCont3.length; i++) {
+  lisCont3[i].style.backgroundColor = "grey";
+  lisCont3[i].textContent = `List Baru ${i + 1}`;
+  lisCont3[i].style.fontFamily = "Comic Sans MS";
+}
